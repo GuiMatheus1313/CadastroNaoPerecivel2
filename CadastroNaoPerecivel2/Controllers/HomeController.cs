677b14bf25ea86850bb899da1970cd0f6e850b4f
GuiMatheus1313 <guilherme.araujo147@etec.sp.gov.br>
2021-06-14 20:32:40 -0300

    Add project files.

---
+++ CadastroNaoPerecivel2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CadastroNaoPerecivel2.Models;
+using System.Collections.ObjectModel;
+
+namespace CadastroNaoPerecivel2.Controllers
+{
+    public class NaoPerecivelController : Controller
+    {
+        public ActionResult Index()
+        {
+            var naoperecivel = new Nao_Pericivel();
+            return View(naoperecivel);
+        }
+        [HttpPost]
+
+        public ActionResult Index(Nao_Pericivel naoperecivel)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", naoperecivel);
+            }
+            return View(naoperecivel);
+        }
+
+        public ActionResult Resultado(Nao_Pericivel naoperecivel)
+        {
+            return View(naoperecivel);
+        }
+    }
+}
---
+++ CadastroNaoPerecivel2/Models/Nao_Perecivel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CadastroNaoPerecivel2.Models
+{
+    public class Nao_Pericivel
+    {
+        [Required(ErrorMessage = "O campo Código é obrigatório")]
+        [Range(0, 1000, ErrorMessage = "O campo Código aceita de 0 a 1000")]
+        [Display(Name = "Nº Código")]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo deve apresentar entre 2 a 30 caracteres")]
+        [Display(Name = "Nome do Produto")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Descrição é obrigatório")]
+        [MaxLength(100, ErrorMessage = "A descrição não deve ultrapassar 100 caracteres")]
+        [Display(Name = "Descrição do Produto")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O campo Peso é obrigatório")]
+        [Range(0.00, 100.00, ErrorMessage = "É aceito o produto apenas entre 0 a 100 KG")]
+        [Display(Name = "Peso do Produto (KG)")]
+        public double Peso { get; set; }
+
+        [Required(ErrorMessage = "O campo Categoria é obrigatório")]
+        [MaxLength(20, ErrorMessage = "A categoria do produto não deve ultrapassar 20 caracteres")]
+        [Display(Name = "Categoria do Produto")]
+        public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "O campo Valor Unitário é obrigatório")]
+        [Range(0.00, 200.00, ErrorMessage = "O valor do produto só pode ser inserido entre R$0,00 à R$200,00")]
+        [Display(Name = "Valor Unitário do Produto")]
+        public double ValorUni { get; set; }
+    }
+}
